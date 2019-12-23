9998c40447ec6eb223fcdc66b3ed40f15078f195
portella94 <chrystian100@hotmail.com>
2019-12-23 17:52:12 -0300

    Christmas commit

---
+++ Assets/Scripts/CameraFocusController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using Vuforia;
+
+public class CameraFocusController : MonoBehaviour
+{
+
+    void Start()
+    {
+        var vuforia = VuforiaARController.Instance;
+        vuforia.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+        vuforia.RegisterOnPauseCallback(OnPaused);
+    }
+
+    private void OnVuforiaStarted()
+    {
+        CameraDevice.Instance.SetFocusMode(
+            CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+    }
+
+    private void OnPaused(bool paused)
+    {
+        if (!paused)
+        {
+            CameraDevice.Instance.SetFocusMode(
+                CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+        }
+    }
+}
---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void EstouNoMuseu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void NaoEstouNoMuseu()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
+    }
+    public void Sair()
+    {
+        Application.Quit();
+    }
+}
